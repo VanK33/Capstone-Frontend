8f079b44b80e6ee6a48d988cdc9f1084f469b697
Jeff Ma <jeff.ma713@gmail.com>
2023-08-30 16:28:00 -0700

    setuping public page

---
+++ src/components/SelectionCard/SelectionCard.jsx
@@ -0,0 +1,23 @@
+function SelectionCard(props) {
+  return (
+    <div>
+      <div>
+        <h3> Origin </h3>
+      </div>
+      <div>
+        <h3> Taste </h3>
+      </div>
+      <div>
+        <h3> Meat-type </h3>
+      </div>
+      <div>
+        <h3> Ingredients </h3>
+      </div>
+      <div>
+        <h3> Results </h3>
+      </div>
+    </div>
+  );
+}
+
+export default SelectionCard;
--- src/pages/PublicPage/PublicPage.jsx
+++ src/pages/PublicPage/PublicPage.jsx
@@ -1,5 +1,18 @@
+import Header from "../../components/Header/Header";
+import SelectionCard from "../../components/SelectionCard/SelectionCard";
+
 function PublicPage() {
-  return;
+  return (
+    <div>
+      <Header />
+      <div>
+        <img> this is the logo </img>
+        <h1> This is the title </h1>
+      </div>
+
+      <SelectionCard />
+    </div>
+  );
 }
 
 export default PublicPage;
